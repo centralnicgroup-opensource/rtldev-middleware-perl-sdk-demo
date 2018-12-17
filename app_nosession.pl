b89deed27bbccbf0601516570bdbbae95932d800
Kai Schwarz <kschwarz@hexonet.net>
2018-12-17 15:57:04 +0100

    Create app_nosession.pl

---
+++ app_nosession.pl
@@ -0,0 +1,31 @@
+use 5.026_000;
+use strict;
+use warnings;
+use WebService::Hexonet::Connector;
+
+my $cl = WebService::Hexonet::Connector::APIClient->new();
+$cl->useOTESystem();
+$cl->setCredentials("test.user", "test.passw0rd");
+$cl->setRemoteIPAddress("1.2.3.4");
+# in case of 2FA use:
+# $cl->setOTP("12345678")
+
+# Call a command
+my $response = $cl->request(
+    {
+        COMMAND => "QueryDomainList",
+        LIMIT => 5
+    }
+);
+
+# get the result in the format you want
+my $res;
+$res = $response->getListHash();
+$res = $response->getHash();
+$res = $response->getPlain();
+
+# get the response code and the response description
+my $code = $response->code();
+my $description = $response->description();
+
+print "$code $description";
